56e811b6b4b61aaf8e64fbf2aa507b37bb7abea4
emin elekberli <elekberli1996@gmail.com>
2024-06-08 23:16:55 +0400

    update

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home.views import home_page
+from home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("list/", list_page,name="list"),
+    path("add/", add_page,name="add"),
+    path("detail/<int:id>", download_pdf,name="detail"),
     path("", home_page,name="home")
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, render,redirect
 from .form import PortfolioForm
+from .models import Portfolio
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import pdfkit
 
 
-def home_page(request):
+
+
+def add_page(request):
 
     form=PortfolioForm(request.POST or None)
 
@@ -15,9 +21,40 @@ def home_page(request):
     context={
         "form":form
     }
-
-
-
-
-    return render(request,"index.html",context)
+    return render(request,"add.html",context)
 # Create your views here.
+
+def list_page(request):
+    users=Portfolio.objects.all()
+    return render(request,"list.html",{'users':users})
+
+
+def home_page(request):
+    return render(request,"index.html")
+
+def detail_page(request,id):
+    user=get_object_or_404(Portfolio,id=id)
+    context={
+        "user":user,
+        
+    }
+
+    return render(request,"detail.html",context)
+
+def download_pdf(request,id):
+    user=get_object_or_404(Portfolio,id=id)
+    context={
+        "user":user,
+        
+    }
+    # Render HTML template as string
+    html_template = render_to_string('detail.html',context)
+
+    # Create PDF
+    pdf = pdfkit.from_string(html_template, False)
+
+    # Prepare HTTP response with PDF
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="etrafli_melumat.pdf"'
+
+    return response
